5900cd04d1c760c96e302999a3d7d743c2d66117
Greg Olmschenk <greg@olmschenk.com>
2021-08-23 16:59:41 -0400

    Use correct model during inference of phase amplitudes

--- ml4a/infer.py
+++ ml4a/infer.py
@@ -5,6 +5,7 @@ from tensorflow.keras.models import Model
 
 from ml4a.nicer_phase_amplitudes_to_parameters_models import Mira
 from ml4a.paths import user_data_directory
+from ml4a.residual_model import ResModel1InitialDenseNoDoConvEndDoublingWidererL2
 
 
 def load_trained_phase_amplitudes_to_parameters_model() -> Model:
@@ -16,7 +17,7 @@ def load_trained_phase_amplitudes_to_parameters_model() -> Model:
 
 
 def load_trained_parameters_to_phase_amplitudes_model() -> Model:
-    model = Mira()
+    model = ResModel1InitialDenseNoDoConvEndDoublingWidererL2()
     model.load_weights(
         user_data_directory.joinpath('model_states/infer_from_phase_amplitudes_to_parameters_model_state/model.ckpt')
     ).expect_partial()
