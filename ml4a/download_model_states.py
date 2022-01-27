e11594c2500a6ad21b8feee7745489bc2a9ef7d1
Greg Olmschenk <greg@olmschenk.com>
2022-01-27 17:09:30 -0500

    Automatically download model states if missing

--- ml4a/download_model_states.py
+++ ml4a/download_model_states.py
@@ -1,15 +1,13 @@
 import warnings
 from pathlib import Path
-from typing import Optional
 
 import dvc.api
 
 from ml4a.paths import user_data_directory
 
 
-def download_model_states(directory: Optional[Path] = None):
-    if directory is None:
-        directory = user_data_directory
+def download_model_states():
+    directory = user_data_directory
     model_state_file_paths = [
         Path('model_states/infer_from_phase_amplitudes_to_parameters_model_state/model.ckpt.data-00000-of-00001'),
         Path('model_states/infer_from_phase_amplitudes_to_parameters_model_state/model.ckpt.index'),
--- ml4a/infer.py
+++ ml4a/infer.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import numpy as np
 from tensorflow.keras.models import Model
 
+from ml4a.download_model_states import download_model_states
 from ml4a.nicer_phase_amplitudes_to_parameters_models import Mira
 from ml4a.paths import user_data_directory
 from ml4a.residual_model import ResModel1InitialDenseNoDoConvEndDoublingWidererL2
@@ -10,11 +11,10 @@ from ml4a.residual_model import ResModel1InitialDenseNoDoConvEndDoublingWidererL
 
 def load_trained_phase_amplitudes_to_parameters_model() -> Model:
     model = Mira()
-    model_path_string = 'model_states/infer_from_phase_amplitudes_to_parameters_model_state/model.ckpt'
-    if Path(model_path_string).exists():
-        model_path = Path(model_path_string)
-    else:
-        model_path = user_data_directory.joinpath(model_path_string)
+    model_path = user_data_directory.joinpath(
+        'model_states/infer_from_phase_amplitudes_to_parameters_model_state/model.ckpt')
+    if not model_path.parent.exists():
+        download_model_states()
     model.load_weights(
         model_path
     ).expect_partial()
@@ -23,11 +23,10 @@ def load_trained_phase_amplitudes_to_parameters_model() -> Model:
 
 def load_trained_parameters_to_phase_amplitudes_model() -> Model:
     model = ResModel1InitialDenseNoDoConvEndDoublingWidererL2()
-    model_path_string='model_states/infer_from_parameters_to_phase_amplitudes_model_state/model.ckpt'
-    if Path(model_path_string).exists():
-        model_path = Path(model_path_string)
-    else:
-        model_path = user_data_directory.joinpath(model_path_string)
+    model_path = user_data_directory.joinpath(
+        'model_states/infer_from_parameters_to_phase_amplitudes_model_state/model.ckpt')
+    if not model_path.parent.exists():
+        download_model_states()
     model.load_weights(
         model_path
     ).expect_partial()
