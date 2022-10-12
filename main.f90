4d3389ef29a44aaea37a8206413f4a990dec3fd4
Greg Olmschenk <greg@olmschenk.com>
2022-10-12 10:54:07 -0400

    Rename the parameters in the Fortran main file to make the values more clear

--- main.f90
+++ main.f90
@@ -2,29 +2,29 @@ program example_using_infer_from_parameters_to_phase_amplitudes
     use, intrinsic :: iso_c_binding, only : c_ptr, c_float, c_int, c_loc
     implicit none
     interface
-        subroutine infer_from_parameters_to_phase_amplitudes(input_array_pointer, output_array_pointer) &
+        subroutine infer_from_parameters_to_phase_amplitudes(parameters_array_pointer, phase_amplitudes_array_pointer) &
                 bind(c, name = 'infer_from_parameters_to_phase_amplitudes')
             import :: c_ptr
-            type(c_ptr), intent(in), value :: input_array_pointer
-            type(c_ptr), intent(in), value :: output_array_pointer
+            type(c_ptr), intent(in), value :: parameters_array_pointer
+            type(c_ptr), intent(in), value :: phase_amplitudes_array_pointer
         end subroutine
     end interface
-    real(c_float), dimension(:), allocatable, target :: input_array(:)
-    real(c_float), dimension(:), allocatable, target :: output_array(:)
-    type(c_ptr) :: input_array_pointer_
-    type(c_ptr) :: output_array_pointer_
+    real(c_float), dimension(:), allocatable, target :: parameters_array(:)
+    real(c_float), dimension(:), allocatable, target :: phase_amplitudes_array(:)
+    type(c_ptr) :: parameters_array_pointer_
+    type(c_ptr) :: phase_amplitudes_array_pointer_
     integer, parameter :: number_of_parameters = 11
     integer, parameter :: number_of_phase_amplitudes = 64
-    allocate(input_array(number_of_parameters))
-    allocate(output_array(number_of_phase_amplitudes))
-    input_array_pointer_ = c_loc(input_array(1))
-    output_array_pointer_ = c_loc(output_array(1))
-    input_array = (/ -0.137349282472716, 4.651922986569446E-002, -0.126309026142708, 2.57614122691645, &
+    allocate(parameters_array(number_of_parameters))
+    allocate(phase_amplitudes_array(number_of_phase_amplitudes))
+    parameters_array_pointer_ = c_loc(parameters_array(1))
+    phase_amplitudes_array_pointer_ = c_loc(phase_amplitudes_array(1))
+    parameters_array = (/ -0.137349282472716, 4.651922986569446E-002, -0.126309026142708, 2.57614122691645, &
             3.94358482944553, 0.303202923979724, 0.132341360556433, 0.304479697430865, 0.758863131388038, &
             3.84855473811096, 2.77055893884855 /)
     write(*, *) "Parameters:"
-    write(*, *) input_array
-    call infer_from_parameters_to_phase_amplitudes(input_array_pointer_, output_array_pointer_)
+    write(*, *) parameters_array
+    call infer_from_parameters_to_phase_amplitudes(parameters_array_pointer_, phase_amplitudes_array_pointer_)
     write(*, *) "Phase amplitudes:"
-    write(*, *) output_array
+    write(*, *) phase_amplitudes_array
 end program example_using_infer_from_parameters_to_phase_amplitudes
