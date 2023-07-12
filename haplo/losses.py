6cb783196bdfeaa7a1a8805c29715d1810a57ddd
golmschenk <greg@olmschenk.com>
2023-07-12 17:39:47 -0400

    Use unnormalized light curves to calculate chi squared statistic

--- haplo/losses.py
+++ haplo/losses.py
@@ -1,11 +1,14 @@
 import torch
-from torch.nn import Module,CrossEntropyLoss
+from torch.nn import Module
+
+from haplo.nicer_transform import PrecomputedUnnormalizePhaseAmplitudes
 
 
 class PlusOneChiSquaredStatisticLoss(Module):
     def forward(self, output: torch.Tensor, target: torch.Tensor):
-        observed = (target + 1.0).type(torch.float64)
-        expected = (output + 1.0).type(torch.float64)
+        unnormalize_phase_amplitudes = PrecomputedUnnormalizePhaseAmplitudes()
+        observed = unnormalize_phase_amplitudes((target + 1.0).type(torch.float64))
+        expected = unnormalize_phase_amplitudes((output + 1.0).type(torch.float64))
         chi_squared_statistic_f64 = torch.mean(torch.sum(((observed - expected) ** 2) / expected, dim=1))
         chi_squared_statistic = chi_squared_statistic_f64.type(torch.float32)
         return chi_squared_statistic
