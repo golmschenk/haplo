13086b64570e0f7616aa7a9be78ea4748889ece6
golmschenk <greg@olmschenk.com>
2023-07-12 12:09:11 -0400

    Refactor from a root level training script to a function

--- haplo/nicer_parameters_to_phase_amplitudes_train.py
+++ haplo/nicer_parameters_to_phase_amplitudes_train.py
@@ -1,47 +1,67 @@
+import random
+
+import numpy as np
 import torch
+from bokeh.io import show
 from torch.nn import Module
 from torch.optim import Adam
 from torch.utils.data import DataLoader
+from bokeh.plotting import figure as Figure
 
 from haplo.losses import PlusOneChiSquaredStatisticLoss
 from haplo.models import LiraTraditionalShape8xWidthWith0d5DoNoBn
 from haplo.nicer_dataset import NicerDataset, split_into_train_validation_and_test_datasets
 from haplo.nicer_transform import PrecomputedNormalizeParameters, PrecomputedNormalizePhaseAmplitudes
 
-if torch.backends.mps.is_available():
-    device = torch.device('mps')
-elif torch.cuda.is_available():
-    device = torch.device('cuda')
-else:
-    device = torch.device('cpu')
 
-full_dataset = NicerDataset(parameters_transform=PrecomputedNormalizeParameters(),
-                            phase_amplitudes_transform=PrecomputedNormalizePhaseAmplitudes())
-train_dataset, validation_dataset, test_dataset = split_into_train_validation_and_test_datasets(full_dataset)
-parameters0, phase_amplitudes0 = train_dataset[0]
+def train_session():
+    if torch.backends.mps.is_available():
+        device = torch.device('mps')
+    elif torch.cuda.is_available():
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
 
-learning_rate = 1e-4
-batch_size = 1000
-epochs = 5000
+    full_dataset = NicerDataset(parameters_transform=PrecomputedNormalizeParameters(),
+                                phase_amplitudes_transform=PrecomputedNormalizePhaseAmplitudes())
+    train_dataset, validation_dataset, test_dataset = split_into_train_validation_and_test_datasets(full_dataset)
+    parameters0, phase_amplitudes0 = train_dataset[0]
 
-train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
-test_dataloader = DataLoader(validation_dataset, batch_size=batch_size)
+    learning_rate = 1e-4
+    batch_size = 1000
+    epochs = 5000
 
-model = LiraTraditionalShape8xWidthWith0d5DoNoBn()
-model = model.to(device)
-loss_function = PlusOneChiSquaredStatisticLoss()
-loss_function = loss_function.to(device)
-optimizer = Adam(model.parameters(), lr=learning_rate)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
+    validation_dataloader = DataLoader(validation_dataset, batch_size=batch_size)
 
 
-def train_loop(dataloader, model_, loss_fn, optimizer):
+    model = LiraTraditionalShape8xWidthWith0d5DoNoBn()
+    model = model.to(device)
+    loss_function = PlusOneChiSquaredStatisticLoss()
+    optimizer = Adam(model.parameters(), lr=learning_rate)
+
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train_loop(train_dataloader, model, loss_function, optimizer, device=device)
+        loop_test(validation_dataloader, model, loss_function, device=device)
+        with torch.no_grad():
+            X_, y_ = test_dataset[random.randrange(100)]
+            pred_y_ = np.squeeze(model(torch.tensor(X_).to(device)).to('cpu').numpy())
+            figure = Figure()
+            figure.line(x=np.arange(64), y=y_, line_color='mediumblue')
+            figure.line(x=np.arange(64), y=pred_y_, line_color='firebrick')
+            show(figure)
+    print("Done!")
+
+
+def train_loop(dataloader, model_, loss_fn, optimizer, device):
     size = len(dataloader.dataset)
     for batch, (X, y) in enumerate(dataloader):
         # Compute prediction and loss
         X = X.to(device)
-        y = y.to(device)
+        y = y
         pred = model_(X)
-        loss = loss_fn(pred, y)
+        loss = loss_fn(pred.to('cpu'), y).to(device)
 
         # Backpropagation
         optimizer.zero_grad()
@@ -53,7 +73,7 @@ def train_loop(dataloader, model_, loss_fn, optimizer):
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
-def loop_test(dataloader, model_: Module, loss_fn):
+def loop_test(dataloader, model_: Module, loss_fn, device):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     test_loss, correct = 0, 0
@@ -61,15 +81,13 @@ def loop_test(dataloader, model_: Module, loss_fn):
     with torch.no_grad():
         for X, y in dataloader:
             X = X.to(device)
-            y = y.to(device)
+            y = y
             pred = model_(X)
-            test_loss += loss_fn(pred, y).item()
+            test_loss += loss_fn(pred.to('cpu'), y).to(device).item()
 
     test_loss /= num_batches
     print(f"Test Error: \nAvg loss: {test_loss:>8f} \n")
 
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train_loop(train_dataloader, model, loss_function, optimizer)
-    loop_test(test_dataloader, model, loss_function)
-print("Done!")
+
+if __name__ == '__main__':
+    train_session()
