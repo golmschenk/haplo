bcb7f161ecc25b58b1868bd4619d7ebcd6918b7c
golmschenk <greg@olmschenk.com>
2023-07-12 17:26:34 -0400

    Use two different files for train and evaluation datasets

--- haplo/data_paths.py
+++ haplo/data_paths.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
 rotated_dataset_path = Path('data/640m_rotated_parameters_and_phase_amplitudes.arrow')
-constantinos_kalapotharakos_format_rotated_dataset_path = Path('data/mcmc_vac_all_version2.dat')
+constantinos_kalapotharakos_format_rotated_dataset_path = Path('data/mcmc_vac_all_640m_A.dat')
 unrotated_dataset_path = Path('data/800k_parameters_and_phase_amplitudes.arrow')
-constantinos_kalapotharakos_format_unrotated_dataset_path = Path('data/mcmc_vac_all_version2.dat')
+constantinos_kalapotharakos_format_unrotated_dataset_path = Path('data/mcmc_vac_all_800k.dat')
