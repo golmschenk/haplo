d731ebc0af62ca9da49309858904d5d758bea74e
golmschenk <greg@olmschenk.com>
2023-07-14 11:29:36 -0400

    Move dataset to NVMe before training

--- haplo/nicer_parameters_to_phase_amplitudes_train.py
+++ haplo/nicer_parameters_to_phase_amplitudes_train.py
@@ -6,7 +6,7 @@ from torch.nn import Module, DataParallel
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 
-from haplo.data_paths import rotated_dataset_path, unrotated_dataset_path
+from haplo.data_paths import rotated_dataset_path, unrotated_dataset_path, move_path_to_nvme
 from haplo.losses import PlusOneChiSquaredStatisticLoss
 from haplo.models import LiraTraditionalShape8xWidthWithNoDoNoBn
 from haplo.nicer_dataset import NicerDataset, split_dataset_into_fractional_datasets
@@ -17,6 +17,7 @@ def train_session():
     wandb.init(project='haplo', entity='ramjet', settings=wandb.Settings(start_method='fork'))
     cpu_count = multiprocessing.cpu_count()
     gpu_count = torch.cuda.device_count()
+    print(f'GPUs: {gpu_count}')
 
     if torch.backends.mps.is_available():
         device = torch.device('mps')
@@ -25,17 +26,19 @@ def train_session():
     else:
         device = torch.device('cpu')
 
+    rotated_dataset_path_moved = move_path_to_nvme(rotated_dataset_path)
+    unrotated_dataset_path_moved = move_path_to_nvme(unrotated_dataset_path)
     train_dataset = NicerDataset(
-        dataset_path=rotated_dataset_path,
+        dataset_path=rotated_dataset_path_moved,
         parameters_transform=PrecomputedNormalizeParameters(),
         phase_amplitudes_transform=PrecomputedNormalizePhaseAmplitudes())
     evaluation_dataset = NicerDataset(
-        dataset_path=unrotated_dataset_path,
+        dataset_path=unrotated_dataset_path_moved,
         parameters_transform=PrecomputedNormalizeParameters(),
         phase_amplitudes_transform=PrecomputedNormalizePhaseAmplitudes())
     validation_dataset, test_dataset = split_dataset_into_fractional_datasets(evaluation_dataset, [0.5, 0.5])
 
-    learning_rate = 1e-4
+    learning_rate = 1e-5
     if gpu_count == 0:
         batch_size = 1000
     else:
@@ -55,7 +58,7 @@ def train_session():
     loss_function = PlusOneChiSquaredStatisticLoss()
     optimizer = Adam(model.parameters(), lr=learning_rate)
 
-    wandb.run.notes = f"pt_{type(model).__name__}_chi_squared_loss_50m_dataset_small_batch_clip_norm_1_cont1"
+    wandb.run.notes = f"pt_{type(model).__name__}_chi_squared_loss_64m_clip_lr_1e-5"
 
     for t in range(epochs):
         print(f"Epoch {t + 1}\n-------------------------------")
@@ -81,7 +84,7 @@ def train_loop(dataloader, model_, loss_fn, optimizer, device, epoch):
 
         if batch % 100 == 0:
             loss_value, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss_value:>7f}  [{current:>5d}/{size:>5d}]")
+            print(f"loss: {loss_value:>7f}  [{current:>5d}/{size:>5d}]", flush=True)
     wandb.log({'plus_one_chi_squared_statistic': loss.item()}, step=epoch)
     wandb.log({'epoch': epoch}, step=epoch)
 
@@ -98,7 +101,7 @@ def loop_test(dataloader, model_: Module, loss_fn, device, epoch):
             test_loss += loss_fn(pred.to('cpu'), y).to(device).item()
 
     test_loss /= num_batches
-    print(f"Test Error: \nAvg loss: {test_loss:>8f} \n")
+    print(f"Test Error: \nAvg loss: {test_loss:>8f} \n", flush=True)
     wandb.log({'val_plus_one_chi_squared_statistic': test_loss}, step=epoch)
 
 
