bf13eae2094e45cc77e0c6e6b06a75beaa0ce450
golmschenk <greg@olmschenk.com>
2023-07-17 21:48:41 -0400

    Correct which value is used as expected and observed in chi squared calculations

--- haplo/losses.py
+++ haplo/losses.py
@@ -7,8 +7,8 @@ from haplo.nicer_transform import PrecomputedUnnormalizePhaseAmplitudes
 class PlusOneChiSquaredStatisticLoss(Module):
     def forward(self, output: torch.Tensor, target: torch.Tensor):
         unnormalize_phase_amplitudes = PrecomputedUnnormalizePhaseAmplitudes()
-        observed = unnormalize_phase_amplitudes((target + 1.0).type(torch.float64))
-        expected = unnormalize_phase_amplitudes((output + 1.0).type(torch.float64))
+        observed = unnormalize_phase_amplitudes((output + 1.0).type(torch.float64))
+        expected = unnormalize_phase_amplitudes((target + 1.0).type(torch.float64))
         chi_squared_statistic_f64 = torch.mean(torch.sum(((observed - expected) ** 2) / expected, dim=1))
         chi_squared_statistic = chi_squared_statistic_f64.type(torch.float32)
         return chi_squared_statistic
