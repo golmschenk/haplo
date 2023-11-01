88bd8aa32e05f07d08bde61602ce93c607906189
golmschenk <greg@olmschenk.com>
2023-11-01 17:18:41 -0400

    Correct dataset tests

--- tests/test_nicer_dataset.py
+++ tests/test_nicer_dataset.py
@@ -1,46 +1,51 @@
+from pathlib import Path
+
 import pandas as pd
 import sqlite3
 
 from haplo.data_column_name import DataColumnName
 from haplo.nicer_dataset import NicerDataset, split_dataset_into_count_datasets, split_dataset_into_fractional_datasets
 
+# TODO: The test datasets should be in memory, not saved to disk.
+database_path = Path(__file__ + '.temp.db')
 
-def create_fake_data():
-    connection = sqlite3.connect(':memory:')
+
+def create_fake_data(rows: int = 3):
+    database_path.unlink(missing_ok=True)
+    database_path.parent.mkdir(exist_ok=True, parents=True)
+    connection = sqlite3.connect(database_path)
     fake_data = []
-    for index in range(3):
+    for index in range(rows):
         fake_data.append({name: name_index + (64 * index) for name_index, name in enumerate(DataColumnName)})
     data_frame = pd.DataFrame(fake_data)
-    data_frame.to_sql(name='main', con=connection)
-    return connection
+    data_frame.to_sql(name='main', con=connection, index=False)
 
 
 def test_getitem():
-    connection = create_fake_data()
-    dataset = NicerDataset(':memory:')
+    create_fake_data()
+    dataset = NicerDataset(database_path)
     parameters1, phase_amplitudes1 = dataset[1]
     assert parameters1[3] == 67
     assert phase_amplitudes1[3] == 78
 
 
 def test_len():
-    fake_data = []
-    connection = create_fake_data()
-    dataset = NicerDataset(':memory:')
+    create_fake_data()
+    dataset = NicerDataset(database_path)
     assert len(dataset) == 3
 
 
 def test_len_after_factional_split():
-    connection = create_fake_data()
-    full_dataset = NicerDataset(':memory:')
+    create_fake_data(rows=8)
+    full_dataset = NicerDataset(database_path)
     fractional_dataset0, fractional_dataset1 = split_dataset_into_fractional_datasets(full_dataset, [0.25, 0.75])
     assert len(fractional_dataset0) == 2
     assert len(fractional_dataset1) == 6
 
 
 def test_len_after_count_split():
-    connection = create_fake_data()
-    full_dataset = NicerDataset(':memory:')
+    create_fake_data(rows=8)
+    full_dataset = NicerDataset(database_path)
     count_dataset0, count_dataset1, count_dataset2 = split_dataset_into_count_datasets(full_dataset, [2, 5])
     assert len(count_dataset0) == 2
     assert len(count_dataset1) == 5
