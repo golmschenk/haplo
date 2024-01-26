34379004d51f07fb793536cf98f50f7dc734b7a7
golmschenk <greg@olmschenk.com>
2024-01-26 15:34:57 -0500

    Correct bug for environment variable being a string instead of path and bump version to 0.7.1

--- src/haplo/train_logging_configuration.py
+++ src/haplo/train_logging_configuration.py
@@ -31,6 +31,8 @@ class TrainLoggingConfiguration:
         if session_directory_environment_variable is not None and session_directory is not None:
             raise ValueError(f'Passing a `session_directory` is not allowed when the environment variable '
                              f'`HAPLO_SESSION_DIRECTORY` is set.')
+        if isinstance(session_directory_environment_variable, str):
+            session_directory_environment_variable = Path(session_directory_environment_variable)
         session_directory = session_directory_environment_variable
         if session_directory is None:
             datetime_string = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
