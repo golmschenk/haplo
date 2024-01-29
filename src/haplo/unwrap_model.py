29d28eb9d0030b2dcc14e70fa28435e3234f49fe
golmschenk <greg@olmschenk.com>
2024-01-29 15:34:08 -0500

    Correct unwrap

--- src/haplo/unwrap_model.py
+++ src/haplo/unwrap_model.py
@@ -2,11 +2,11 @@ from collections import OrderedDict
 
 
 def unwrap_model(model_dict):
-    unwrapped_model_dict = OrderedDict
+    unwrapped_model_dict = OrderedDict()
     for key, value in model_dict.items():
         if key.startswith('module.'):
             unwrapped_key = key[7:]
         else:
             unwrapped_key = key
-        unwrapped_model_dict.update({unwrapped_key: value})
+        unwrapped_model_dict[unwrapped_key] = value
     return unwrapped_model_dict
