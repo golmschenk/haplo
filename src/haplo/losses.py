c732917e65250201983be5eac7a271eb7f343904
golmschenk <greg@olmschenk.com>
2024-02-23 14:07:16 -0500

    Add SumDifferenceSquaredOverMedianExpectedSquaredMetric metric

--- src/haplo/losses.py
+++ src/haplo/losses.py
@@ -26,5 +26,19 @@ class PlusOneBeforeUnnormalizationChiSquaredStatisticMetric(Module):
         return chi_squared_statistic
 
 
+class SumDifferenceSquaredOverMedianExpectedSquaredMetric(Module):
+    def forward(self, output: torch.Tensor, target: torch.Tensor):
+        epsilon = 1e-10
+        unnormalize_phase_amplitudes = PrecomputedUnnormalizePhaseAmplitudes()
+        observed = unnormalize_phase_amplitudes(output.type(torch.float64)) + 1.0
+        expected = unnormalize_phase_amplitudes(target.type(torch.float64)) + 1.0
+        numerator = torch.sum(((observed - expected) ** 2), dim=1)
+        median = torch.median(expected, dim=1).values
+        denominator = median ** 2
+        quality_indicator = numerator / denominator
+        metric_f64 = torch.mean(torch.log10(quality_indicator + epsilon))
+        metric = metric_f64.type(torch.float32)
+        return metric
+
 def norm_based_gradient_clip(gradient_tensor: Tensor) -> Tensor:
     return gradient_tensor / torch.maximum(torch.linalg.vector_norm(gradient_tensor), torch.tensor(1.0))
