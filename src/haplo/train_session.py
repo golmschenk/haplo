9020de79b1af16745910aa5e092dc0805d443bf0
golmschenk <greg@olmschenk.com>
2024-10-11 10:13:06 -0400

    Fix wandb start method for Windows

--- src/haplo/train_session.py
+++ src/haplo/train_session.py
@@ -1,3 +1,5 @@
+import platform
+
 import getpass
 import logging
 import math
@@ -49,8 +51,13 @@ def train_session(train_dataset: Dataset, validation_dataset: Dataset, model: Mo
     logger.info('Starting training...')
     logging_configuration.session_directory.mkdir(exist_ok=True, parents=True)
     local_rank, process_rank, world_size = get_distributed_world_information()
+
+    if platform.system() == 'Windows':
+        wandb_start_method = 'thread'
+    else:
+        wandb_start_method = 'fork'
     wandb_init(process_rank=process_rank, project=logging_configuration.wandb_project,
-               entity=logging_configuration.wandb_entity, settings=wandb.Settings(start_method='fork'))
+               entity=logging_configuration.wandb_entity, settings=wandb.Settings(start_method=wandb_start_method))
     wandb_log_data_class(hyperparameter_configuration, process_rank=process_rank)
     wandb_log_data_class(system_configuration, process_rank=process_rank)
     wandb_log_dictionary(logging_configuration.additional_log_dictionary, process_rank=process_rank)
