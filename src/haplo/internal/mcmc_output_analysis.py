Correct log-likelihood median obtaining to work with Dask arrays

src/haplo/internal/mcmc_output_analysis.py
@@ -86,10 +86,10 @@ def extract_windowed_median_log_likelihood_series(dataset: xarray.Dataset, windo
     :param window_size: The size of the window to take the medians over.
     :return: The resulting medians. The Pandas Series has an index that is the start of the window.
     """
-    bin_edges = np.concat([
+    bin_edges = np.concatenate([
         np.arange(dataset['iteration'].min(), dataset['iteration'].max() + 1, window_size, dtype=np.int64),
         np.array([dataset['iteration'].max() + 1], dtype=np.int64)
     ])
     binned = dataset['log_likelihood'].groupby_bins('iteration', bins=bin_edges, right=False)
-    medians = binned.median(dim=['iteration', 'cpu', 'chain']).compute().to_numpy()
+    medians = binned.quantile(q=0.5, dim=['iteration', 'cpu', 'chain']).compute().to_numpy()
     return pd.Series(index=bin_edges[:-1], data=medians)