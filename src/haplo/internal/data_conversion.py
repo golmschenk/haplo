25f9aca9d89758b3d72825b344b77fea6d53c660
golmschenk <greg@olmschenk.com>
2025-10-07 13:56:50 -0400

    Remove duplicate memory map function

--- src/haplo/internal/data_conversion.py
+++ src/haplo/internal/data_conversion.py
@@ -1,24 +1,21 @@
 from __future__ import annotations
 
-from enum import StrEnum
 import itertools
 import logging
-import math
 import mmap
-from typing import TextIO
-
-import numpy as np
-import pandas as pd
 import re
 import shutil
+from enum import StrEnum
+from pathlib import Path
+
+import pandas as pd
 import xarray
 import zarr
 from pandas import DataFrame
-from pathlib import Path
 from xarray import Dataset
-from zarr.storage import ZipStore, StorePath, LocalStore
+from zarr.storage import ZipStore
 
-from haplo.data_preparation import arbitrary_constantinos_kalapotharakos_file_path_to_pandas
+from haplo.internal.constantinos_kalapotharakos_format import get_memory_mapped_file_contents
 from haplo.logging import set_up_default_logger
 
 logger = logging.getLogger(__name__)
@@ -218,9 +215,3 @@ def convert_from_2d_xarray_zarr_to_csv(xarray_zarr_path: Path, csv_path: Path) -
     dataset = xarray.open_zarr(xarray_zarr_path)
     data_frame: pd.DataFrame = to_ordered_dataframe(dataset)
     data_frame.to_csv(csv_path, index=False)
-
-
-def get_memory_mapped_file_contents(file_handle: TextIO) -> mmap.mmap:
-    file_fileno = file_handle.fileno()
-    file_contents = mmap.mmap(file_fileno, 0, access=mmap.ACCESS_READ)
-    return file_contents
