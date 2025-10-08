ab4b3edb28f299e74fd68fcfb22b9892fd800e29
golmschenk <greg@olmschenk.com>
2025-10-07 22:56:04 -0400

    Remove old losses with normalizations

--- src/haplo/internal/losses.py
+++ src/haplo/internal/losses.py
@@ -1,43 +1,6 @@
 import torch
 from torch.nn import Module
 
-from haplo.nicer_transform import PrecomputedUnnormalizePhaseAmplitudes
-
-
-class PlusOneChiSquaredStatisticMetricWithNormalization(Module):
-    def forward(self, output: torch.Tensor, target: torch.Tensor):
-        unnormalize_phase_amplitudes = PrecomputedUnnormalizePhaseAmplitudes()
-        observed = unnormalize_phase_amplitudes(output.type(torch.float64)) + 1.0
-        expected = unnormalize_phase_amplitudes(target.type(torch.float64)) + 1.0
-        chi_squared_statistic_f64 = torch.mean(torch.sum(((observed - expected) ** 2) / expected, dim=1))
-        chi_squared_statistic = chi_squared_statistic_f64.type(torch.float32)
-        return chi_squared_statistic
-
-
-class PlusOneBeforeUnnormalizationChiSquaredStatisticMetricWithNormalization(Module):
-    def forward(self, output: torch.Tensor, target: torch.Tensor):
-        unnormalize_phase_amplitudes = PrecomputedUnnormalizePhaseAmplitudes()
-        observed = unnormalize_phase_amplitudes(output.type(torch.float64) + 1.0)
-        expected = unnormalize_phase_amplitudes(target.type(torch.float64) + 1.0)
-        chi_squared_statistic_f64 = torch.mean(torch.sum(((observed - expected) ** 2) / expected, dim=1))
-        chi_squared_statistic = chi_squared_statistic_f64.type(torch.float32)
-        return chi_squared_statistic
-
-
-class SumDifferenceSquaredOverMedianExpectedSquaredMetricWithNormalization(Module):
-    def forward(self, output: torch.Tensor, target: torch.Tensor):
-        epsilon = 1e-10
-        unnormalize_phase_amplitudes = PrecomputedUnnormalizePhaseAmplitudes()
-        observed = unnormalize_phase_amplitudes(output.type(torch.float64)) + 1.0
-        expected = unnormalize_phase_amplitudes(target.type(torch.float64)) + 1.0
-        numerator = torch.sum(((observed - expected) ** 2), dim=1)
-        median = torch.median(expected, dim=1).values
-        denominator = median ** 2
-        quality_indicator = numerator / denominator
-        metric_f64 = torch.mean(torch.log10(quality_indicator + epsilon))
-        metric = metric_f64.type(torch.float32)
-        return metric
-
 
 class PlusOneChiSquaredStatisticMetric(Module):
     def forward(self, output: torch.Tensor, target: torch.Tensor):
