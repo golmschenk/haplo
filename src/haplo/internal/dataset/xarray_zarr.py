cfe67b53af6242864d2c8dcab8c01ccab2810bdb
golmschenk <greg@olmschenk.com>
2025-10-08 11:18:03 -0400

    Add memory caching

--- src/haplo/internal/dataset/xarray_zarr.py
+++ src/haplo/internal/dataset/xarray_zarr.py
@@ -2,18 +2,22 @@ import xarray
 from pathlib import Path
 from typing import Self
 from xarray import Dataset
-from zarr.storage import ZipStore, LocalStore
+from zarr.experimental.cache_store import CacheStore
+from zarr.storage import ZipStore, LocalStore, MemoryStore
 
 from haplo.internal.sized_dataset import SizedDataset
 
 
 class XarrayBasedDataset(SizedDataset):
     @classmethod
-    def new(cls, zarr_path: Path) -> Self:
+    def new(cls, zarr_path: Path, memory_cached: bool = True) -> Self:
         if zarr_path.suffix == '.zip':
             store = ZipStore(zarr_path)
         else:
             store = LocalStore(zarr_path)
+        if memory_cached:
+            memory_store = MemoryStore()
+            store = CacheStore(store=store, cache_store=memory_store)
         xarray_dataset: Dataset = xarray.open_zarr(store)
         instance = cls(xarray_dataset=xarray_dataset)
         return instance
