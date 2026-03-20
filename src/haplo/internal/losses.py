3c90f6d784efd1be6b0c59522e59ab164287151b
golmschenk <greg@olmschenk.com>
2026-03-20 18:40:36 -0400

    Add metric variants

--- src/haplo/internal/losses.py
+++ src/haplo/internal/losses.py
@@ -25,3 +25,71 @@ class SumDifferenceSquaredOverMedianExpectedSquaredMetric(Module):
         return metric
 
 
+class SumDifferencePowerOverMedianExpectedPowerMetric(Module):
+    def forward(self, output: torch.Tensor, target: torch.Tensor):
+        epsilon = 1e-10
+        observed = output.type(torch.float64) + 1.0
+        expected = target.type(torch.float64) + 1.0
+        numerator = torch.sum((torch.abs(observed - expected) ** 1.1), dim=1)
+        median = torch.median(expected, dim=1).values
+        denominator = median ** 1.1
+        quality_indicator = numerator / denominator
+        metric_f64 = torch.mean(torch.log10(quality_indicator + epsilon))
+        metric = metric_f64.type(torch.float32)
+        return metric
+
+
+class SumDifferenceSquaredOverMedianExpectedSquaredWithLogExpectedMetric(Module):
+    def forward(self, output: torch.Tensor, target: torch.Tensor):
+        epsilon = 1e-10
+        observed = output.type(torch.float64) + 1.0
+        expected = torch.log(target.type(torch.float64) + 1.0)
+        numerator = torch.sum(((observed - expected) ** 2), dim=1)
+        median = torch.median(expected, dim=1).values
+        denominator = median ** 2
+        quality_indicator = numerator / denominator
+        metric_f64 = torch.mean(torch.log10(quality_indicator + epsilon))
+        metric = metric_f64.type(torch.float32)
+        return metric
+
+
+class SumDifferenceSquaredOverMedianExpectedSquaredWithLogExpectedMetricNoLogInQi(Module):  # TODO: This one
+    def forward(self, output: torch.Tensor, target: torch.Tensor):
+        epsilon = 1e-10
+        observed = output.type(torch.float64)
+        expected = torch.log(target.type(torch.float64) + 2.0)
+        numerator = torch.sum(((observed - expected) ** 2), dim=1)
+        median = torch.median(expected, dim=1).values
+        denominator = median ** 2
+        quality_indicator = numerator / denominator
+        metric_f64 = torch.mean(quality_indicator + epsilon)
+        metric = metric_f64.type(torch.float32)
+        return metric
+
+
+class SumDifferenceSquaredOverMedianExpectedSquaredWithExpObservedMetric(Module):  # TODO: This one
+    def forward(self, output: torch.Tensor, target: torch.Tensor):
+        epsilon = 1e-10
+        observed = torch.exp(output.type(torch.float64))
+        expected = target.type(torch.float64) + 2.0
+        numerator = torch.sum(((observed - expected) ** 2), dim=1)
+        median = torch.median(expected, dim=1).values
+        denominator = median ** 2
+        quality_indicator = numerator / denominator
+        metric_f64 = torch.mean(torch.log10(quality_indicator + epsilon))
+        metric = metric_f64.type(torch.float32)
+        return metric
+
+
+class SumDifferenceSquaredOverMedianExpectedSquaredWithExpObservedMetricLse(Module):
+    def forward(self, output: torch.Tensor, target: torch.Tensor):
+        epsilon = 1e-10
+        observed = torch.exp(output.type(torch.float64)) + 1.0
+        expected = target.type(torch.float64) + 1.0
+        numerator = torch.sum(((observed - expected) ** 2), dim=1)
+        median = torch.median(expected, dim=1).values
+        denominator = median ** 2
+        quality_indicator = numerator / denominator
+        metric_f64 = torch.mean(torch.log10(quality_indicator + epsilon))
+        metric = metric_f64.type(torch.float32)
+        return metric
